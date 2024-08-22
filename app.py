b5d98c7c1ec5257d2230831e3ff17b0fe8feb73d
Aruloci <dominic.pfister@swissonline.ch>
2024-08-22 14:19:50 +0200

    Suppressed logs

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
+import logging
 from modules import create_app
 
 app = create_app()
 
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
