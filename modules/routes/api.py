ad1ae75e785b688113d37a5647097d7661b899cc
Aruloci <dominic.pfister@swissonline.ch>
2024-09-22 17:02:49 +0200

    fixed items not being accepted with dev versions

--- modules/routes/api.py
+++ modules/routes/api.py
@@ -283,6 +283,9 @@ def shutdown_worker():
     worker_thread.join()
     
 def compare_versions(version_a: str, version_b: str) -> bool:
+    if version_a.lower().find("dev") != 1:
+        return True
+    
     # Split the versions by dot and convert each part to an integer
     parts_a = list(map(int, version_a.split('.')))
     parts_b = list(map(int, version_b.split('.')))
