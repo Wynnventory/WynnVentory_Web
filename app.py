bdbb257781f80e58bc1237c827829b5df9f6414d
Aruloci <dominic.pfister@swissonline.ch>
2024-10-01 11:35:45 +0200

    Removed CORS and adjusted URL

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 import logging
 from modules import create_app
-from flask_cors import CORS
 
 app = create_app()
 
-CORS(app, resources={r"/api/*": {"origins": "https://www.wynnventory.com"}})
 
 log = logging.getLogger('werkzeug')
 # log.setLevel(logging.WARNING)
