77036bf828ff5d47c17a96baf0061727cb94d91c
Aruloci <dominic.pfister@swissonline.ch>
2024-10-01 12:22:27 +0200

    Refactored and added history to nav

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,21 +1,43 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
+
+ALLOWED_IPS = ["127.0.0.1",
+               "83.76.209.66", # Pfister PC
+               "178.197.215.137", # Tim Handy
+               "85.2.46.237", # Tim PC
+               "178.197.211.252" # Pfister Handy
+               ]
+
+
+def is_allowed_ip():
+    """Returns True if the user's IP is in the allowed list."""
+    if request.headers.getlist("X-Forwarded-For"):
+        user_ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        user_ip = request.remote_addr  # Fallback to remote address
+
+    return user_ip in ALLOWED_IPS
 
 
 def create_app():
-    app = Flask(__name__, 
+    app = Flask(__name__,
                 static_url_path='',
-                static_folder='../templates/static', 
+                static_folder='../templates/static',
                 template_folder='../templates')
-    
+
     # ROUTES
     from modules.routes.web import web_bp
     from modules.routes.api import api_bp
-    
+    app.register_blueprint(web_bp)
+    app.register_blueprint(api_bp)
+
     # 404 Error
     @app.errorhandler(404)
     def page_not_found(e):
         return redirect(url_for('web.index'), 404)
 
-    app.register_blueprint(web_bp)
-    app.register_blueprint(api_bp)
+    @app.context_processor
+    def inject_ip_check():
+        """Injects whether the current user's IP is allowed into all templates."""
+        show_price_history = is_allowed_ip()  # Perform IP check
+        return dict(show_price_history=show_price_history)
     return app
--- modules/routes/web.py
+++ modules/routes/web.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, render_template, abort
 from datetime import datetime, timezone
 
-from modules import wynn_api
+from modules import wynn_api, is_allowed_ip
 from modules.routes import api
 
-
 web_bp = Blueprint('web', __name__)
+
 
 @web_bp.route("/")
 @web_bp.route("/index")
@@ -14,10 +14,9 @@ def index():
 
 @web_bp.route("/items")
 def items():
-    # items = wynn_api.get_item_database()
     return render_template("items.html")
 
-@web_bp.route("/item") # TODO: TEST ONLY
+@web_bp.route("/item")  # TODO: TEST ONLY
 def item():
     items = wynn_api.search_item(query="Collapse")
     return render_template("single_item.html", items=items)
@@ -25,13 +24,10 @@ def item():
 @web_bp.route("/lootrun")
 def lootrun_lootpool():
     loot_data = api.get_lootpool_items("lootpool")[0].get_json()
-
-    if not isinstance(loot_data, list):
-        loot_data = []
+    loot_data = loot_data if isinstance(loot_data, list) else []
 
     now = datetime.now(timezone.utc)
-    
-    # Calculate time difference
+
     for item in loot_data:
         timestamp = datetime.strptime(item["timestamp"], '%a, %d %b %Y %H:%M:%S %Z').replace(tzinfo=timezone.utc)
         time_diff = now - timestamp
@@ -40,23 +36,17 @@ def lootrun_lootpool():
             item["last_updated"] = f"Last updated {int(minutes)} minutes ago"
         else:
             hours = minutes // 60
-            if hours == 1:
-                item["last_updated"] = f"Last updated {int(hours)} hour ago"
-            else:
-                item["last_updated"] = f"Last updated {int(hours)} hours ago"
-    
+            item["last_updated"] = f"Last updated {int(hours)} hour{'s' if hours > 1 else ''} ago"
+
     return render_template("lootrun_lootpool.html", loot_data=loot_data)
 
 @web_bp.route("/raid")
 def raid_lootpool():
     loot_data = api.get_lootpool_items("raidpool")[0].get_json()
-
-    if not isinstance(loot_data, list):
-        loot_data = []
+    loot_data = loot_data if isinstance(loot_data, list) else []
 
     now = datetime.now(timezone.utc)
 
-    # Calculate time difference
     for item in loot_data:
         timestamp = datetime.strptime(item["timestamp"], '%a, %d %b %Y %H:%M:%S %Z').replace(tzinfo=timezone.utc)
         time_diff = now - timestamp
@@ -65,10 +55,7 @@ def raid_lootpool():
             item["last_updated"] = f"Last updated {int(minutes)} minutes ago"
         else:
             hours = minutes // 60
-            if hours == 1:
-                item["last_updated"] = f"Last updated {int(hours)} hour ago"
-            else:
-                item["last_updated"] = f"Last updated {int(hours)} hours ago"
+            item["last_updated"] = f"Last updated {int(hours)} hour{'s' if hours > 1 else ''} ago"
 
     return render_template("raid_lootpool.html", loot_data=loot_data)
 
@@ -80,24 +67,8 @@ def players():
 @web_bp.route("/history/<item_name>")
 @web_bp.route("/history/<item_name>/")
 def history(item_name):
-    # List of allowed IP addresses
-    allowed_ips = ["83.76.209.66", "127.0.0.1"]
-
-    
-    # Check if the app is behind a proxy and get the real client IP
-    if request.headers.getlist("X-Forwarded-For"):
-        # 'X-Forwarded-For' may have a list of IPs, we take the first one as the client IP
-        user_ip = request.headers.getlist("X-Forwarded-For")[0]
+    if is_allowed_ip():
+        return render_template("price_history.html", item_name=item_name)
     else:
-        # Use 'remote_addr' if there is no proxy
-        user_ip = request.remote_addr
-
-    print(f"Trying to access price history for {item_name} from {user_ip}")
-    # Check if the user's IP is in the list of allowed IPs
-    if user_ip not in allowed_ips:
-        print(f"IP {user_ip} is not allowed")
-        abort(404)  # Show 404 error if the IP is not allowed
-    
-    
-    # Render the price history page if IP is allowed
-    return render_template("price_history.html", item_name=item_name)
+        print("History was accessed via URL but the IP is not allowed.")
+        abort(404)
