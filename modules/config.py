8d3fb7cf37342f4c9b198aa30ab8844ef442e0c2
Dominic Pfister <dominic.pfister@swissonline.ch>
2025-04-19 15:14:29 +0200

    reworked config.py

--- modules/config.py
+++ modules/config.py
@@ -4,15 +4,10 @@ class Config:
     MIN_SUPPORTED_VERSION = ''
 
     # Database URIs
-    PROD_URI = "mongodb+srv://Test1234:Test1234@wynnventory.9axarep.mongodb.net/"
+    PROD_URI = "mongodb+srv://Test1234:Test1234@wynnventory.9axarep.mongodb.net/wynnventory"
     DEV_URI = "mongodb+srv://Test1234:Test1234@wynnventory.9axarep.mongodb.net/wynnventory_DEV"
+    ADMIN_URI = "mongodb+srv://Test1234:Test1234@wynnventory.9axarep.mongodb.net/wynnventory_admin"
 
     @classmethod
     def get_current_uri(cls):
         return cls.DEV_URI if cls.ENVIRONMENT == "dev" else cls.PROD_URI
-
-    @classmethod
-    def set_environment(cls, env):
-        if env not in ["prod", "dev"]:
-            raise ValueError("Environment must be either 'prod' or 'dev'")
-        cls.ENVIRONMENT = env
--- modules/db.py
+++ modules/db.py
@@ -1,23 +1,29 @@
-import logging
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 from modules.config import Config
 from modules.models.collection_types import Collection
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def get_client(db: str = "current") -> MongoClient:
+    """
+    Returns a MongoClient pointed at:
+     - the current ENVIRONMENT db (if db="current")
+     - the admin db              (if db="admin")
+    """
+    if db == "admin":
+        uri = Config.ADMIN_URI
+    else:
+        uri = Config.get_current_uri()
 
-def get_client():
-    uri = Config.get_current_uri()
-    return MongoClient(uri, server_api=ServerApi('1'), tls=True, tlsAllowInvalidCertificates=True)
+    return MongoClient(
+        uri,
+        server_api=ServerApi("1"),
+        tls=True,
+        tlsAllowInvalidCertificates=True
+    )
 
 def get_collection(collection: Collection):
-    client = get_client()
-    db = client["wynnventory" if Config.ENVIRONMENT == "prod" else "wynnventory_DEV"]
-    
-    if not isinstance(collection, Collection):
-        return None
-        
-    return db[collection.value]
+    client = get_client("admin" if collection is Collection.API_KEYS else "current")
+    db     = client.get_default_database()
+    return db[collection._value_]
