9bb3a3894b1ad164939d0bfaa51b709019859790
Dominic Pfister <dominic.pfister@swissonline.ch>
2025-04-20 12:15:57 +0200

    properly records usage on shutdown

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import atexit
 import os
 
 from modules import create_app
 from modules.config import Config
+from modules.utils.queue_worker import shutdown_workers
 
 app = create_app()
 
@@ -9,4 +11,11 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     debug = Config.ENVIRONMENT != "prod"
     app.logger.info(f"Starting in '{Config.ENVIRONMENT}' mode with min version '{Config.MIN_SUPPORTED_VERSION}')")
-    app.run(host="0.0.0.0", port=port, debug=debug)
+
+    atexit.register(shutdown_workers)
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=debug
+    )
