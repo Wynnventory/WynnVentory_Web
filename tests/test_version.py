b485754aa4d2052ca1c91fa1050d19370859cf54
Tim Killenberger <timkillenberger@gmail.com>
2025-06-08 17:52:23 +0200

    fix: optimized imports

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,6 @@
-import unittest
-import sys
 import os
+import sys
+import unittest
 
 # Add the parent directory to sys.path to import the module
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
